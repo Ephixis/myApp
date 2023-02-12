10e62ca615f296266632c8959799ed07901bb2fc
Ephixis <L.Brinkmoeller@gmx.de>
2023-02-12 19:05:10 +0100

    Initial commit for person-detail-component person-detail-component now displays data of selected person.

---
+++ src/app/person-detail/person-detail.component.ts
@@ -0,0 +1,11 @@
+import { Component, Input } from '@angular/core';
+import { Person } from '../person';
+
+@Component({
+  selector: 'app-person-detail',
+  templateUrl: './person-detail.component.html',
+  styleUrls: ['./person-detail.component.css']
+})
+export class PersonDetailComponent {
+  @Input() person?: Person;
+}
