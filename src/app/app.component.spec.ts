99d8bf894bb16e4cd7eccdd57dae32b08348c1e3
Ephixis <L.Brinkmoeller@gmx.de>
2023-02-16 20:30:24 +0100

    Small test changes for personComonent.

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -20,7 +20,7 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'myApp'`, () => {
+  it(`should have as title 'Personal Verwaltung'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
     expect(app.title).toEqual('myApp');
--- src/app/person/person.component.spec.ts
+++ src/app/person/person.component.spec.ts
@@ -1,23 +1,24 @@
+import { DebugElement } from '@angular/core';
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
 import { PersonComponent } from './person.component';
 
 describe('PersonComponent', () => {
   let component: PersonComponent;
   let fixture: ComponentFixture<PersonComponent>;
+  let de: DebugElement;
 
-  beforeEach(async () => {
-    await TestBed.configureTestingModule({
-      declarations: [ PersonComponent ]
-    })
-    .compileComponents();
+  TestBed.configureTestingModule({
+    declarations: [PersonComponent]
+  }).compileComponents();
 
+  beforeEach(() => {
     fixture = TestBed.createComponent(PersonComponent);
     component = fixture.componentInstance;
+    de = fixture.debugElement;
     fixture.detectChanges();
   });
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
-  });
+  
 });
+
+
